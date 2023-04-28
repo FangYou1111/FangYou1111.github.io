c180548b4baacd834a0156b83dbef2a41876a24b
FangYou <1716532235@qq.com>
2023-04-28 16:12:28 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
-import Vue from 'vue'
-import App from './App.vue'
-
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import typeNav from './components/TypeNav';
+import Carousel from './components/Carousel';
+import Pagination from './components/Pagination';
+import store from './store';
+import './mock/mockServe';
+import "swiper/css/swiper.css";
+import * as API from '@/api/index';
+import {
+  Button, MessageBox
+} from 'element-ui';
+import VueLazyload from 'vue-lazyload';
+import lazyDefaultPicture from '../src/assets/images/1.gif';
+import '@/plugins/validate';
 Vue.config.productionTip = false
-
+Vue.component('typeNav', typeNav);
+Vue.component('carousel', Carousel);
+Vue.component('Pagination', Pagination);
+Vue.component(Button.name, Button);
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$alert = MessageBox.alert;
+Vue.use(VueLazyload, {
+  loading: lazyDefaultPicture
+});
 new Vue({
   render: h => h(App),
+  router,
+  store,
+  beforeCreate() {
+    Vue.prototype.$bus = this;
+    Vue.prototype.$api = API;
+  }
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// module.exports= defineConfig({
+//   transpileDependencies: true
+// });
+module.exports = {
+  lintOnSave: false,//关闭eslint
+  productionSourceMap: false,
+  devServer: {
+    proxy: {
+      'api': {
+        target: 'http://gmall-h5-api.atguigu.cn'
+      }
+    }
+  },
+};
